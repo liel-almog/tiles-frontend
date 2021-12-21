8668bebe902f2d7f824bd21318463f1ed703eb0b
Liel Almog <liel.almog@agamimp.net>
2021-12-21 16:19:09 +0200

    Feat: Add Router

---
+++ src/Router.tsx
@@ -0,0 +1,17 @@
+import { Routes, Route } from "react-router-dom";
+import App from './App'
+
+const Router = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<App />}>
+        {/* <Route index element={<Login />}></Route>
+        <Route path="admin" element={<Admin />}></Route> */}
+        {/* <Route path="tile" element="Tile"></Route> */}
+        {/* <Route path="*" element={<NotFound />}></Route> */}
+      </Route>
+    </Routes>
+  );
+};
+
+export default Router;
