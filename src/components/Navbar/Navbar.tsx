c15bf921c122562647e89bda9b9245d65a0a3b02
Liel Almog <liel.almog@agamimp.net>
2022-01-03 09:45:02 +0200

    Refactor: add active class in better reactish way

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import { Link, useLocation } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 import AuthContext from "../../contexts/auth-context";
 import classNames from "classnames";
 
@@ -9,11 +9,13 @@ export interface NavbarProps {}
 
 export const Navbar: React.VFC<NavbarProps> = () => {
   const { user } = useContext(AuthContext);
-  const location = useLocation();
-  const {onLogout} = useContext(AuthContext)
+  const { onLogout } = useContext(AuthContext);
+
+  const active = ({ isActive }: { isActive: boolean }) =>
+    `${isActive ? classes.active : ""} ${classes.vertical}`;
 
   return (
-    <nav className={classes.nav} >
+    <nav className={classes.nav}>
       <section className={classes.user} onClick={onLogout}>
         <img src="/src/public/user.svg" alt="user" className={classes.img} />
         <article className={classes.info}>
@@ -24,24 +26,12 @@ export const Navbar: React.VFC<NavbarProps> = () => {
 
       {user.role === "Admin" && (
         <section className={classes.links}>
-          <article
-            className={classNames(classes.vertical, {
-              [classes.active]: location.pathname === "/",
-            })}
-          >
-            <Link to="/">
-              <img src="/src/public/tile-managment.svg" alt="tile management" />
-            </Link>
-          </article>
-          <article
-            className={classNames(classes.vertical, {
-              [classes.active]: location.pathname === "/admin",
-            })}
-          >
-            <Link to="/admin">
-              <img src="/src/public/admin-page.svg" alt="admin management" />
-            </Link>
-          </article>
+          <NavLink className={active} to="/">
+            <img src="/src/public/tile-managment.svg" alt="tile management" />
+          </NavLink>
+          <NavLink className={active} to="/admin">
+            <img src="/src/public/admin-page.svg" alt="admin management" />
+          </NavLink>
         </section>
       )}
     </nav>
