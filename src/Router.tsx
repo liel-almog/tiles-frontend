ad29eaa238cdd45e4f079d08761ec151eb14f0a6
Liel Almog <liel.almog@agamimp.net>
2022-01-10 14:13:01 +0200

    Feat: Add protected routes to /admin and /

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,21 +1,29 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate,  } from "react-router-dom";
 import App from "./App";
 import { NotFound } from "./components/NotFound";
 import { Signup } from "./components/Forms/Signup";
 import { Login } from "./components/Forms/Login";
 import { Admin } from "./components/Admin";
 import { TilesList } from "./components/TilesList";
+import AuthContext from "./contexts/auth-context";
+import { useContext } from "react";
 
 const Router = () => {
+  const { user, isLoggedIn } = useContext(AuthContext);
   return (
     <Routes>
       <Route path="/login" element={<Login />}></Route>
       <Route path="/signup" element={<Signup />}></Route>
-      <Route path="/" element={<App />}>
-        <Route index element={<TilesList />}></Route>
-        <Route path="admin" element={<Admin />}></Route>
-        <Route path="*" element={<NotFound />}></Route>
-      </Route>
+      {isLoggedIn && (
+        <Route path="/" element={<App />}>
+          <Route index element={<TilesList />}></Route>
+          {user.role === "Admin" && (
+            <Route path="admin" element={<Admin />}></Route>
+          )}
+          <Route path="*" element={<NotFound />}></Route>
+        </Route>
+      )}
+      <Route path="*" element={<Navigate to={'/login'} />}></Route>
     </Routes>
   );
 };
